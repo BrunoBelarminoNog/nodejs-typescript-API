26f010b2c943fc81c5309d7267eb604d5828dc44
BrunoBelarminoNog <brunobelarm.nog@gmail.com>
2021-06-05 17:01:26 -0300

    feat: [C02P03] configuraçao dinamica com node-config

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,6 @@
 import { InternalError } from './../controllers/errors/internal-error';
 import { AxiosStatic } from 'axios';
+import config, {IConfig} from 'config';
 
 export interface StormGlassPointsSource {
   [key: string]: number;
@@ -47,6 +48,8 @@ export class StormGlassResponseError extends InternalError {
   }
 }
 
+const stormGlassResourceConfig: IConfig = config.get('App.resources.StormGlass')
+
 
 export class StormGlass {
   readonly stormGlassAPIParams =
@@ -58,10 +61,10 @@ export class StormGlass {
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
       try {
         const response = await this.request.get<StormGlassForecastResponse>(
-            `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`,
+            `${stormGlassResourceConfig.get('apiUrl')}/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`,
             {
               headers: {
-                Authorization: 'fake-token',
+                Authorization: stormGlassResourceConfig.get('apiToken'),
               },
             }
           );
